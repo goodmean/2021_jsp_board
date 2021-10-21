94686ed0af09a52cfa6a1e0706116d42be854b95
goodmean <zizigoo12@gmail.com>
2021-10-21 22:35:10 +0900

    게시물 작성 처리 서블릿 생성

---
+++ src/main/java/com/chm/exam/exam2/servlet/UsrArticleDoWriteServlet.java
@@ -0,0 +1,37 @@
+package com.chm.exam.exam2.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/usr/article/doWrite")
+public class UsrArticleDoWriteServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 들어오는 파라미터를 UTF-8로 해석
+		request.setCharacterEncoding("UTF-8");
+		
+		// 서블릿이 HTML 파일을 만들 때 UTF-8 로 쓰기
+		response.setCharacterEncoding("UTF-8");
+		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+		response.setContentType("text/html;charset=utf-8");
+		
+		String title = request.getParameter("title");
+		String body = request.getParameter("body");
+		
+		//response.getWriter().append(title + "<br>");
+		//response.getWriter().append(body + "<br>");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
