72dbc87c9820fa7f0a40549eb6b45cf31659c6ba
goodmean <zizigoo12@gmail.com>
2021-11-01 00:45:23 +0900

    lombok 적용으로 소스코드 다이어트

--- src/main/java/com/chm/exam/exam2/http/controller/UsrArticleController.java
+++ src/main/java/com/chm/exam/exam2/http/controller/UsrArticleController.java
@@ -1,8 +1,15 @@
 package com.chm.exam.exam2.http.controller;
 
+import com.chm.exam.exam2.dto.ResultData;
 import com.chm.exam.exam2.http.Rq;
+import com.chm.exam.exam2.http.service.ArticleService;
 
 public class UsrArticleController extends Controller {
+	private ArticleService articleService;
+
+	public UsrArticleController() {
+		articleService = new ArticleService();
+	}
 
 	@Override
 	public void performAction(Rq rq) {
@@ -19,10 +26,20 @@ public class UsrArticleController extends Controller {
 	private void actionDoWrite(Rq rq) {
 		String title = rq.getParam("title", "");
 		String body = rq.getParam("body", "");
-		
-		rq.printf("title : %s<br>\n", title);
-		rq.printf("body : %s<br>\n", body);
-		
+
+		if (title.length() == 0) {
+			rq.historyBack("title을 입력해주세요.");
+			return;
+		}
+
+		if (body.length() == 0) {
+			rq.historyBack("body를 입력해주세요.");
+			return;
+		}
+
+		ResultData writeRd = articleService.write(title, body);
+
+		rq.printf(writeRd.getMsg());
 	}
 
 	private void actionShowWrite(Rq rq) {
---
+++ src/main/java/com/chm/exam/exam2/http/repository/ArticleRepository.java
@@ -0,0 +1,20 @@
+package com.chm.exam.exam2.http.repository;
+
+import com.chm.mysqlutil.MysqlUtil;
+import com.chm.mysqlutil.SecSql;
+
+public class ArticleRepository {
+	public int write(String title, String body) {
+		SecSql sql = new SecSql();
+		sql.append("INSERT INTO article");
+		sql.append("SET regDate = NOW()");
+		sql.append(", updateDate = NOW()");
+		sql.append(", title = ?", title);
+		sql.append(", `body` = ?", body);
+		
+		int id = MysqlUtil.insert(sql);
+		
+		return id;
+	}
+
+}
